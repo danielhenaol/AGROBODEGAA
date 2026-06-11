3863666aa12853f5785c33bb4fb7b5491d26f90e
Kell531 <kellyfernandauribe@gmail.com>
2026-06-10 21:53:30 -0500

    feat: add spring cloud config server parameter catalog

--- config-server/src/main/java/com/agrobodega/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/agrobodega/configserver/ConfigServerApplication.java
@@ -4,11 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.util.Map;
+
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication app = new SpringApplication(ConfigServerApplication.class);
+
+        app.setDefaultProperties(Map.of(
+                "spring.application.name", "agrobodega-config-server",
+                "server.port", "8888",
+                "spring.profiles.active", "native",
+                "spring.cloud.config.server.native.search-locations", "classpath:/config-repo",
+                "management.endpoints.web.exposure.include", "health,info",
+                "management.endpoint.health.show-details", "always"
+        ));
+
+        app.run(args);
     }
 }
